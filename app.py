483df6a8edaebba80d9492d878714f0034a9cd06
Joshua Karstendick <joshdick@gmail.com>
2019-06-30 21:58:17 -0400

    Add database interaction

--- app.py
+++ app.py
@@ -1,12 +1,43 @@
-# compose_flask/app.py
 from flask import Flask
+import psycopg2
+from psycopg2 import pool
+import os
+
+DB_HOST = os.environ["DB_HOST"]
+DB_PORT = os.environ["DB_PORT"]
+DB_USER = os.environ["DB_USER"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+DB_DATABASE = os.environ["DB_DATABASE"]
 
 app = Flask(__name__)
 
 
+conn_pool = psycopg2.pool.ThreadedConnectionPool(
+    1,
+    2,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    port=DB_PORT,
+    database=DB_DATABASE,
+)
+
+
 @app.route("/hello")
 def hello():
-    return "Hello, World!"
+    try:
+        conn = conn_pool.getconn()
+        cursor = conn.cursor()
+        cursor.execute("select * from foo")
+        recs = cursor.fetchall()
+        s = ""
+        for row in recs:
+            s += str(row)
+        cursor.close()
+        return "Hello, World! " + s
+    finally:
+        if conn and conn_pool:
+            conn_pool.putconn(conn)
 
 
 if __name__ == "__main__":
